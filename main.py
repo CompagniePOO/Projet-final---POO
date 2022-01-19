8da810d8d016e2a3548219b7b76ed6adea99ed8e
AlecFraser1 <83378903+alecfraser3141@users.noreply.github.com>
2022-01-19 18:07:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from PIL import ImageTk, Image  
+import time
+import yfinance
+import os
+
+#Add stock info if time allows it.
+
+class Main():
+  def __init__():
+
+root = Tk()
+
+
+logo = Canvas(root, width = 150, height = 130)
+logo.pack(side=TOP, anchor = NW)
+logoImg = ImageTk.PhotoImage(Image.open("images (1).png"))
+logo.create_image(20, 20, anchor = NW, image = logoImg)
+
+loginBtn = Button(text="LOGIN",fg="red", height = 3, width = 20, command=lambda:print("Hello"))
+loginBtn.place(x=230, y=150)
+
+
+root.geometry("700x360")
+
+root.mainloop()
+
